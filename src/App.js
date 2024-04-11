e7977796c3257d307f947934b3909b0a5bc3f79b
NipunaGamage888 <69056579+NipunaGamage888@users.noreply.github.com>
2024-04-11 02:02:43 -0400

    Header component with nav completed including css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
+import './App.scss';
 import Header from './components/Header/Header';
+import Main from './components/Main/Main';
+import { BrowserRouter, Routes, Route  } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Header/>
+     <BrowserRouter>
+      
+      <Routes>
+       <Route path='/'element={<Main/>}/>
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,24 +7,29 @@ function Header() {
   const [userLoggedIn, setIsUserLoggedIn] = useState(true);
   return (
     <header className="header">
-      <h1 className="header__heading">Parking genie</h1>
-      <nav className="header__nav">
-        <ul className="header__list">
-          <li className="headder__li">About Us</li>
-          <li className="header__li">book now</li>
-          <li
-            className={` ${userLoggedIn ? "header__user-logged" : "header__li"}`}
-          >
-            login
-          </li>
-          <li
-            className={`header__li ${userLoggedIn ? "header__li" : "header__user-loggeds"}`}
-          >
-            Profile
-          </li>
-        </ul>
-      </nav>
-      <Booking/>
+      <div className="header__main">
+        <h1 className="header__heading"></h1>
+        <nav className="header__nav">
+          <ul className="header__list">
+            <li className="header__li">About Us</li>
+            <li className="header__li">book now</li>
+            <li
+              className={` ${
+                userLoggedIn ? "header__user-logged" : "header__li"
+              }`}
+            >
+              login
+            </li>
+            <li
+              className={`header__li ${
+                userLoggedIn ? "header__li" : "header__user-loggeds"
+              }`}
+            >
+              Profile
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
